a93411e5340237552ef7ecc27ff2616f320c39bd
Phillip Nguyen <pnguyen4711@gmail.com>
2021-04-18 15:16:20 -0400

    apply new theme and change fonts + cleanup

--- config.el
+++ config.el
@@ -6,13 +6,28 @@
       user-mail-address "pnguyen4711@gmail.com")
 
 ;; Font config (I make heavy use of italics so I use fonts that support it)
-(setq doom-font (font-spec :family "Liberation Mono" :size 14)
-      doom-big-font (font-spec :family "Liberation Mono" :size 30)
-      doom-serif-font (font-spec :family "IBM Plex Serif" :size 14)
-      doom-variable-pitch-font (font-spec :family "IBM Plex Serif" :size 14))
+(setq doom-font (font-spec :family "Iosevka Extended" :size 15)
+      doom-big-font (font-spec :family "Iosevka Extended" :size 30)
+      doom-serif-font (font-spec :family "Iosevka Slab Extended" :size 15)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Serif"))
 
 ;; Use this function to list available fonts (uncomment and evaluate with "gr"):
-;; (print (font-family-list))
+(print (font-family-list))
+
+;; Custom Theme
+(setq doom-theme 'typeset)
+
+;; Basic UI/UX Changes
+(setq display-line-numbers-type nil)   ; No line numbers
+(setq-default tab-width 4)             ; This is what I like
+(modify-syntax-entry ?_ "w")           ; Treat underscores as word delimiters
+(global-subword-mode 1)                ; Iterate through camelCase words too
+(setq which-key-idle-delay 0.2)        ; Make which-key help popup appear sooner
+(map! "C-S-V" #'yank                   ; Set intuitive copy/paste keybindings
+      "C-S-C" #'kill-ring-save)
+
+;; Projectile Config
+(setq projectile-ignored-projects '("/tmp" "~/.emacs.d/.local/straight/repos/"))
 
 ;; Use mixed pitch for org files, markdown files, and info pages
 (add-hook! (gfm-mode markdown-mode) #'mixed-pitch-mode)
@@ -22,7 +37,7 @@
 ;; Automatically toggle LaTeX fragment previews as the cusor enters/exits them
 (add-hook! 'org-mode-hook #'org-fragtog-mode)
 
-;; Org Mode
+;; Org Mode Config
 (use-package! org-super-agenda
   :after org-agenda
   :config (org-super-agenda-mode))
@@ -35,87 +50,48 @@
         org-agenda-skip-deadline-if-done t
         org-agenda-skip-scheduled-if-done t
         org-agenda-include-deadlines t
-        org-todo-keywords '((sequence "TODO(t)" "INPROGRESS(i)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
         org-fancy-priorities-list '("HIGH" "MID" "LOW")
-        org-super-agenda-groups '((:name "Schedule"  :time-grid t :date today :scheduled today)
+        org-priority-faces '((?A :foreground "#ff0000")
+                             (?C :foreground "#b0ada2"))
+        org-super-agenda-groups '((:name "Schedule"
+                                   :time-grid t
+                                   :date today
+                                   :scheduled today)
                                   (:name "Due today" :deadline today)
                                   (:name "Important" :priority "A")
                                   (:name "Overdue"   :deadline past)
                                   (:name "Due soon"  :deadline future))
-        org-priority-faces '((?A :foreground "#ff0000")
-                             (?C :foreground "#b0ada2"))
-        org-todo-keyword-faces
-        '(("TODO"       :foreground "#7c7c75" :weight normal :underline t)
-          ("WAITING"    :foreground "#9f7efe" :weight normal :underline t)
-          ("INPROGRESS" :foreground "#0098dd" :weight normal :underline t)
-          ("DONE"       :foreground "#50a14f" :weight normal :underline t)
-          ("CANCELLED"  :foreground "#ff6480" :weight normal :underline t))))
-
-;; Relative line numbers
-(setq display-line-numbers-type 'relative)
-
-;; This is what I like
-(setq-default tab-width 4)
-
-;; Treat underscores as word delimiters
-(modify-syntax-entry ?_ "w")
-
-;; Iterate through camelCase words too
-(global-subword-mode 1)
-
-;; Make which-key help popup appear sooner
-(setq which-key-idle-delay 0.2)
-
-;; Keybindings for copy and paste similar to alacritty (e.g. control + shift + v)
-(map! "C-S-V" #'yank
-      "C-S-C" #'kill-ring-save)
-
-;; Projectile Config
-(setq projectile-ignored-projects '("/tmp" "~/.emacs.d/.local/straight/repos/"))
+        org-todo-keywords '((sequence "TODO(t)"
+                                      "INPROGRESS(i)"
+                                      "WAITING(w)"
+                                      "|"
+                                      "DONE(d)"
+                                      "CANCELLED(c)"))
+        org-todo-keyword-faces '(("TODO"
+                                  :foreground "#7c7c75"
+                                  :weight normal
+                                  :underline t)
+                                 ("WAITING"
+                                  :foreground "#9f7efe"
+                                  :weight normal
+                                  :underline t)
+                                 ("INPROGRESS"
+                                  :foreground "#0098dd"
+                                  :weight normal
+                                  :underline t)
+                                 ("DONE"
+                                  :foreground "#50a14f"
+                                  :weight normal
+                                  :underline t)
+                                 ("CANCELLED"
+                                  :foreground "#ff6480"
+                                  :weight normal
+                                  :underline t))))
 
 ;; Quality of life fixes for EIN
 (setq ein:output-area-inlined-images t)
 (setq ein:polymode t)
 (map! :map ein:notebook-mode-map "C-c C-\\" #'ein:worksheet-execute-all-cells)
-
-;; Theme + modifications
-(setq doom-theme 'tao-yang)
-(custom-theme-set-faces!
-  'tao-yang
-
-  ;; Don't change underlying font foreground on highlight line
-  '(hl-line :background "#FAF7EE" :foreground nil :extend t)
-
-  ;; Boxes make which-key menu ugly
-  '(font-lock-function-name-face :box nil :slant italic)
-
-  ;; EIN specifics
-  `(ein:cell-input-area :background "#faf7ee")
-
-  ;; Markdown Mode Specifics
-  '(markdown-markup-face :inherit markdown-language-keyword-face)
-  '(markdown-code-face   :extend t :background "#faf7ee")
-  '(markdown-language-keyword-face :background nil :foreground "#b0ada2")
-  '(markdown-header-face-1 :height 1.50 :weight extra-bold :inherit markdown-header-face)
-  '(markdown-header-face-2 :height 1.40 :weight bold       :inherit markdown-header-face)
-  '(markdown-header-face-3 :height 1.30 :weight bold       :inherit markdown-header-face)
-  '(markdown-header-face-4 :height 1.20 :weight bold       :inherit markdown-header-face)
-  '(markdown-header-face-5 :height 1.10 :weight bold       :inherit markdown-header-face)
-  '(markdown-header-face-6 :height 1.00 :weight bold       :inherit markdown-header-face)
-
-  ;; Org Mode Specifics
-  '(org-document-title :height 1.50 :weight bold)
-  '(org-level-1        :height 1.30 :weight bold)
-  '(org-level-2        :height 1.10 :weight bold)
-  '(org-level-3        :height 1.00 :weight bold)
-  '(org-level-4        :height 1.00 :weight bold)
-  '(org-level-5        :height 1.00 :weight bold)
-  '(org-level-6        :height 1.00 :weight bold)
-  '(org-meta-line :extend t :background "#edeae2")
-  '(org-block-begin-line    :background "#edeae2")
-  '(org-block-end-line      :background "#edeae2")
-  '(org-block               :background "#faf7ee"))
-  '(org-headline-done :weight normal :strike-through t :foreground "#b0ada2")
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
